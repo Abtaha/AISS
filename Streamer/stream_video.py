af03212e03cfb98a2566beb273816fa31c2699cd
FoxUnderGround <54197021+FoxUnderGround@users.noreply.github.com>
2020-03-08 22:10:32 +0500

    Add files via upload

--- Streamer/stream_video.py
+++ Streamer/stream_video.py
@@ -1,6 +1,7 @@
 import time
 import cv2
 from flask import Flask, render_template, Response
+cap = cv2.VideoCapture(0)
 
 app = Flask(__name__)
 
@@ -12,23 +13,22 @@ def index():
 
 def gen():
     """Video streaming generator function."""
-    cap = cv2.VideoCapture(0)
 
     # Read until video is completed
-    while(cap.isOpened()):
+    #while(cap.isOpened()):
       # Capture frame-by-frame
-        ret, img = cap.read()
-        if ret == True:
-            #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img = cv2.resize(img, (500,500))
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    ret, img = cap.read()
+    if ret == True:
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (500,500))
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             #time.sleep(0.1)
-        else:
-            break
+        #else:
+        #    break
 
 
-@app.route('/video_feed')
+@app.route('/video_feed.jpg')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
