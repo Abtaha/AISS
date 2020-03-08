7d6d04510c6a5da1ba4206b83db16eb5ed62db60
FoxUnderGround <54197021+FoxUnderGround@users.noreply.github.com>
2020-03-08 22:29:09 +0500

    Update Neural Networks.py

--- AI System/Neural Networks.py
+++ AI System/Neural Networks.py
@@ -1,9 +1,29 @@
 import time
 import cv2
 import numpy as np
+import urllib.request
 
 from models.Face_Recognition import Face_Recognition
 from models.object_detection_lite.objDetection import run
+
+def get_stream():
+    stream = urllib.request.urlopen('http://192.168.10.7:5000/video_feed.jpg')
+    total_bytes = b''
+    i = 0
+    while True:
+        total_bytes += stream.read(1024)
+        b = total_bytes.find(b'\xff\xd9') # JPEG end
+        if not b == -1:
+            a = total_bytes.find(b'\xff\xd8') # JPEG start
+            jpg = total_bytes[a:b+2] # actual image
+            total_bytes= total_bytes[b+2:] # other informations
+
+            # decode to colored image ( another option is cv2.IMREAD_GRAYSCALE )
+            img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            i = 1 # display image while receiving data
+            if cv2.waitKey(1) ==27 or i == 1: # if user hit esc
+                break
+    return img
 
 class NeuralNets:
     # Basic Initialization
@@ -25,13 +45,15 @@ class NeuralNets:
         self.objectDetection(frame)
 
 NN = NeuralNets()
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
 while True:
     start = time.time()
 
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (500,500))
+    #ret, frame = cap.read()
+    #frame = cv2.resize(frame, (500,500))
+    
+    frame=get_stream()
     
     NN.run(frame)
     
@@ -42,5 +64,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
